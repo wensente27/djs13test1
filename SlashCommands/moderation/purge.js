c33fee5491dfc0ceca0012d1b4cf231c78e03679
wensente27 <jahpxpanique@gmail.com>
2021-10-05 23:15:15 +0800

    Add more checkers

--- SlashCommands/moderation/purge.js
+++ SlashCommands/moderation/purge.js
@@ -2,11 +2,15 @@ const { MessageActionRow } = require("discord.js")
 
 module.exports.run = async (inter) => {
     let amount = inter.options.getNumber('amount')
-    if(amount <= 100) {
+
+    if (amount > 100 || amount < 1) {
+        return inter.reply("Please select a number **between 100 and 1**.")
+    } else if(amount <= 100) {
         inter.channel.bulkDelete(amount, true)
         return await inter.reply(`Successfully purged **\`${amount}\`** messages.`)
     }
-    
+
+   
 }
 
 module.exports.help = {
