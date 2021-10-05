aa7a42881f70d568bb6bf799159e7b4f7ff3ede8
wensente27 <jahpxpanique@gmail.com>
2021-10-05 22:40:24 +0800

    Remove Context Menu thingy

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,7 +2,6 @@ const Client = require('../index').Client
 Client.on('interactionCreate', async inter => {
     if(inter.isCommand()) {
         let slashCmds = Client.SlashCmds.get(inter.commandName)
-        if(!inter.member.permissions.has(slashCmds.help.memberPermissions)) return
         if(slashCmds) slashCmds.run(inter)
     }
 })
